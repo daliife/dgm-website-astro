a4e8cd2e60c85c6990f57de4853aba94dbf9f2e6
David <davidgimeno1992@gmail.com>
2024-04-16 00:33:44 +0200

    fix: Update astro.config.mjs with corrected site URL

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,18 @@
 import { defineConfig } from "astro/config";
-
 import tailwind from "@astrojs/tailwind";
 
+const SERVER_PORT = 4321;
+const LOCALHOST_URL = `http://localhost:${SERVER_PORT}`;
+const LIVE_URL = "https://daliife.github.io/dgm-website-astro/";
+const SCRIPT = process.env.npm_lifecycle_script || "";
+const isBuild = SCRIPT.includes("astro build");
+let BASE_URL = LOCALHOST_URL;
+if (isBuild) {
+  BASE_URL = LIVE_URL;
+}
+
 export default defineConfig({
+  server: { port: SERVER_PORT },
+  site: BASE_URL,
   integrations: [tailwind()],
-  site: "https://dgm-website-astro.github.io",
-  // base: "/dgm-website-astro",
 });
