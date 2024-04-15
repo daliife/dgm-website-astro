138009147f6cf225404ff1bcfb720e4115009eb8
David <davidgimeno1992@gmail.com>
2024-04-16 00:56:38 +0200

    Update LIVE_URL in astro.config.mjs to use the correct GitHub Pages URL

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,7 +4,7 @@ import tailwind from "@astrojs/tailwind";
 
 const SERVER_PORT = 4321;
 const LOCALHOST_URL = `http://localhost:${SERVER_PORT}`;
-const LIVE_URL = "https://daliife.github.io/dgm-website-astro/";
+const LIVE_URL = "https://dgm-website-astro.github.io";
 const SCRIPT = process.env.npm_lifecycle_script || "";
 const isBuild = SCRIPT.includes("astro build");
 let BASE_URL = LOCALHOST_URL;
