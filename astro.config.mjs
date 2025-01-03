d61d7e4c28928c092bc6039a47f4e5f56207db54
David <davidgimeno1992@gmail.com>
2025-01-03 13:15:15 +0100

    chore: Update LIVE_URL in astro.config.mjs for new website path

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,7 +5,7 @@ import react from "@astrojs/react";
 
 const SERVER_PORT = 4321;
 const LOCALHOST_URL = `http://localhost:${SERVER_PORT}`;
-const LIVE_URL = "https://daliife.github.io";
+const LIVE_URL = "https://daliife.github.io/dgm-website-astro";
 const SCRIPT = process.env.npm_lifecycle_script || "";
 const isBuild = SCRIPT.includes("astro build");
 let BASE_URL = LOCALHOST_URL;
