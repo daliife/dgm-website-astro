c0ab63ffce3cb0ae9ab4404b02b765e3e6c94b3b
David <davidgimeno1992@gmail.com>
2025-01-03 12:56:54 +0100

    chore: Update LIVE_URL in astro.config.mjs to point to new GitHub Pages site

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,9 +2,10 @@ import { defineConfig } from "astro/config";
 import sitemap from "@astrojs/sitemap";
 import tailwind from "@astrojs/tailwind";
 import react from "@astrojs/react";
+
 const SERVER_PORT = 4321;
 const LOCALHOST_URL = `http://localhost:${SERVER_PORT}`;
-const LIVE_URL = "https://dgm-website-astro.github.io";
+const LIVE_URL = "https://daliife.github.io/dgm-website-astro";
 const SCRIPT = process.env.npm_lifecycle_script || "";
 const isBuild = SCRIPT.includes("astro build");
 let BASE_URL = LOCALHOST_URL;
@@ -12,7 +13,6 @@ if (isBuild) {
   BASE_URL = LIVE_URL;
 }
 
-// https://astro.build/config
 export default defineConfig({
   server: {
     port: SERVER_PORT,
