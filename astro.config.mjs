6d0591d42bb6b743bac3d4cee2d694ef8f5cf723
David <davidgimeno1992@gmail.com>
2025-01-03 14:02:20 +0100

    chore: Update LIVE_URL in astro.config.mjs and adjust icon and image paths in Layout and index pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,7 +5,7 @@ import react from "@astrojs/react";
 
 const SERVER_PORT = 4321;
 const LOCALHOST_URL = `http://localhost:${SERVER_PORT}`;
-const LIVE_URL = "https://daliife.github.io/dgm-website-astro";
+export const LIVE_URL = "http://davidgimeno.cat";
 const SCRIPT = process.env.npm_lifecycle_script || "";
 const isBuild = SCRIPT.includes("astro build");
 let BASE_URL = LOCALHOST_URL;
@@ -14,11 +14,10 @@ if (isBuild) {
 }
 
 export default defineConfig({
+  site: BASE_URL,
+  integrations: [sitemap(), tailwind(), react()],
+  output: "static",
   server: {
     port: SERVER_PORT,
   },
-  site: BASE_URL,
-  base: "/dgm-website-astro",
-  integrations: [sitemap(), tailwind(), react()],
-  output: "static",
 });
